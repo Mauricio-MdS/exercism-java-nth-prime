a240788478029884a203a8724e16b641a0e8a8ec
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-05-23 11:16:59 -0300

    Exercise solution

--- src/main/java/PrimeCalculator.java
+++ src/main/java/PrimeCalculator.java
@@ -1,7 +1,23 @@
 class PrimeCalculator {
 
     int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (nth < 1) throw new IllegalArgumentException("Argument must be above 0.");
+
+        int candidate =  2;
+
+        for (int count = 0; count < nth; candidate++)
+            if (isPrime(candidate)) count++;
+
+        return candidate - 1;
+    }
+
+    boolean isPrime(int candidate) {
+        int sqrt = (int) Math.sqrt(candidate);
+
+        for (int i = 2; i <= sqrt; i++)
+            if (candidate % i == 0) return false;
+
+        return true;
     }
 
 }
--- src/test/java/PrimeCalculatorTest.java
+++ src/test/java/PrimeCalculatorTest.java
@@ -1,9 +1,7 @@
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 public class PrimeCalculatorTest {
 
@@ -14,25 +12,21 @@ public class PrimeCalculatorTest {
         assertThat(primeCalculator.nth(1)).isEqualTo(2);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testSecondPrime() {
         assertThat(primeCalculator.nth(2)).isEqualTo(3);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testSixthPrime() {
         assertThat(primeCalculator.nth(6)).isEqualTo(13);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testBigPrime() {
         assertThat(primeCalculator.nth(10001)).isEqualTo(104743);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testUndefinedPrime() {
         assertThatExceptionOfType(IllegalArgumentException.class)
